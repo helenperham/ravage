9295d362076632c63c7328a7666686114f63e263
Helen <helenmperham@gmail.com>
2023-03-29 16:44:48 -0400

    imported aboutpage to app

--- src/aboutpage.jsx
+++ src/aboutpage.jsx
@@ -1,24 +1,20 @@
 import react from 'react'
-import { Route, Routes } from "react-router-dom"
 import './global.css'
 import NavBar from './navbar'
-import Homepage from './homepage'
-import Aboutpage from './aboutpage'
 // import {layout} from './layout.jsx'
 // import {footer} from './footer.jsx' - haven't added this file yet, but will add in 
 // purpose of footer is to hold the subscription to mailchimp link
 
-function App({data, layout, footer}) {
+function Homepage({data, layout, footer}) {
   console.log(data)
   return (
     <div>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/about" element={<Aboutpage />} />
-      </Routes>
+        <h1>
+          <p>About page </p>
+        </h1>
+
       </div>
   )
 }
 
-export default App
+export default Homepage
