0d89bc0eda6e728424ea5c0727c22ee047a2ca5a
Helen <helenmperham@gmail.com>
2023-04-04 15:54:41 -0400

    added the about page info

--- src/aboutpage.jsx
+++ src/aboutpage.jsx
@@ -12,6 +12,11 @@ function Homepage({data, layout, footer}) {
         <h1>
           <p>About page </p>
         </h1>
+        <>
+          <p>Ravage is a 5 year project orchestrated by Tiff Gum Ying Cheng. Many of the scenes in it depended on a mood of nervous triumph, especially since the script was handwritten and put into some order using tape and was therefore almost of no help.
+
+Email us: porkornopork at g-popular email service dot com.</p>
+        </>
 
       </div>
   )
